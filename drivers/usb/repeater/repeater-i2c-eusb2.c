b36b4a4064f723829913e56490bc26c061a31891
kmiit <kmiit@foxmail.com>
2025-10-26 12:23:05 +0800

    stash
    Change-Id: I4fb4c90543f7e5a7e0bbb8bf5a513fb6ba53ea27

--- drivers/usb/repeater/repeater-i2c-eusb2.c
+++ drivers/usb/repeater/repeater-i2c-eusb2.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2024, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/debugfs.h>
@@ -18,6 +18,7 @@
 #include <linux/regulator/consumer.h>
 #include <linux/types.h>
 #include <linux/usb/repeater.h>
+#include <linux/usb/dwc3-msm.h>
 
 #define EUSB2_3P0_VOL_MIN			3075000 /* uV */
 #define EUSB2_3P0_VOL_MAX			3300000 /* uV */
@@ -89,6 +90,8 @@ struct eusb2_repeater {
 	struct gpio_desc		*reset_gpiod;
 	u32				*param_override_seq;
 	u8				param_override_seq_cnt;
+	u32				*param_override_seq_host;
+	u8				param_override_seq_cnt_host;
 };
 
 static const struct regmap_config eusb2_i2c_regmap = {
@@ -96,6 +99,7 @@ static const struct regmap_config eusb2_i2c_regmap = {
 	.val_bits = 8,
 	.max_register = 0xff,
 };
+
 
 static int eusb2_i2c_read_reg(struct eusb2_repeater *er, u8 reg, u8 *val)
 {
@@ -114,7 +118,7 @@ static int eusb2_i2c_read_reg(struct eusb2_repeater *er, u8 reg, u8 *val)
 	return 0;
 }
 
-static int eusb2_i2c_write_reg(struct eusb2_repeater *er, u8 reg, u8 val)
+static int eusb2_i2c_write_reg(struct eusb2_repeater *er, u8 reg, u32 val)
 {
 	int ret;
 
@@ -241,7 +245,7 @@ err_vdd18:
 	return ret;
 }
 
-static int eusb2_repeater_init(struct usb_repeater *ur)
+static int eusb2_repeater_init(struct usb_repeater *ur, unsigned int flags)
 {
 	struct eusb2_repeater *er =
 			container_of(ur, struct eusb2_repeater, ur);
@@ -265,11 +269,18 @@ static int eusb2_repeater_init(struct usb_repeater *ur)
 	dev_info(er->ur.dev, "eUSB2 repeater version = 0x%x ur->flags:0x%x\n", reg_val, ur->flags);
 
 	/* override init sequence using devicetree based values */
-	if (er->param_override_seq_cnt)
-		eusb2_repeater_update_seq(er, er->param_override_seq,
+	if (er->param_override_seq_cnt) {
+		if (flags & PHY_HOST_MODE) {
+			/*it's host, then write host eye params*/
+			eusb2_repeater_update_seq(er, er->param_override_seq_host,
+					er->param_override_seq_cnt_host);
+			dev_info(er->ur.dev, "eUSB2 repeater init host \n");
+		} else {
+			eusb2_repeater_update_seq(er, er->param_override_seq,
 					er->param_override_seq_cnt);
-
-	dev_info(er->ur.dev, "eUSB2 repeater init\n");
+			dev_info(er->ur.dev, "eUSB2 repeater init device!\n");
+		}
+	}
 
 	return 0;
 }
@@ -405,10 +416,40 @@ static int eusb2_repeater_i2c_probe(struct i2c_client *client)
 		}
 	}
 
+	/*This is for host params. start*/
+	num_elem = of_property_count_elems_of_size(dev->of_node, "qcom,param-override-seq-host",
+				sizeof(*er->param_override_seq_host));
+	if (num_elem > 0) {
+		if (num_elem % 2) {
+			dev_err(dev, "invalid param_override_seq_len\n");
+			ret = -EINVAL;
+			goto err_probe;
+		}
+
+		er->param_override_seq_cnt_host = num_elem;
+		er->param_override_seq_host = devm_kcalloc(dev,
+				er->param_override_seq_cnt_host,
+				sizeof(*er->param_override_seq_host), GFP_KERNEL);
+		if (!er->param_override_seq_host) {
+			ret = -ENOMEM;
+			goto err_probe;
+		}
+
+		ret = of_property_read_u32_array(dev->of_node,
+				"qcom,param-override-seq-host",
+				er->param_override_seq_host,
+				er->param_override_seq_cnt_host);
+		if (ret) {
+			dev_err(dev, "qcom,param-override-seq-host read failed %d\n",
+									ret);
+			goto err_probe;
+		}
+	}
+	/*This is for host params. end*/
 
 	er->ur.dev = dev;
 
-	er->ur.init		= eusb2_repeater_init;
+	er->ur.init_nxp		= eusb2_repeater_init;
 	er->ur.reset		= eusb2_repeater_reset;
 	er->ur.powerup		= eusb2_repeater_powerup;
 	er->ur.powerdown	= eusb2_repeater_powerdown;
@@ -416,6 +457,7 @@ static int eusb2_repeater_i2c_probe(struct i2c_client *client)
 	ret = usb_add_repeater_dev(&er->ur);
 	if (ret)
 		goto err_probe;
+	pr_info("%s success.\n", __func__);
 
 	return 0;
 
@@ -427,8 +469,12 @@ static void eusb2_repeater_i2c_remove(struct i2c_client *client)
 {
 	struct eusb2_repeater *er = i2c_get_clientdata(client);
 
+	if (!er)
+		return;
+
 	usb_remove_repeater_dev(&er->ur);
 	eusb2_repeater_power(er, false);
+	return;
 }
 
 static struct i2c_driver eusb2_i2c_repeater_driver = {
@@ -443,4 +489,4 @@ static struct i2c_driver eusb2_i2c_repeater_driver = {
 module_i2c_driver(eusb2_i2c_repeater_driver);
 
 MODULE_DESCRIPTION("eUSB2 i2c repeater driver");
-MODULE_LICENSE("GPL");
+MODULE_LICENSE("GPL v2");
